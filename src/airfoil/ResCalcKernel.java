c1db02a03d97d5bf581cf2f62c15c673fe45fab6
Kyrill Tkachov <kyrilltkachov@yandex.ru>
2012-01-08 15:17:59 +0000

    added processing of host code halo data

--- src/airfoil/ResCalcKernel.java
+++ src/airfoil/ResCalcKernel.java
@@ -26,13 +26,8 @@ public class ResCalcKernel extends Kernel {
 	private final int input_data_count_width = 32;
 	private final HWType input_data_count_t = hwUInt(input_data_count_width);
 	private final int partition_size = 1<<10;
+	private final int halo_size = 1<<7;
 
-//	private final KStructType.StructFieldType x1_f = KStructType.sft("x1", array2_t);
-//	private final KStructType.StructFieldType x2_f = KStructType.sft("x2", array2_t);
-//	private final KStructType.StructFieldType q1_f = KStructType.sft("q1", array4_t);
-//	private final KStructType.StructFieldType q2_f = KStructType.sft("q2", array4_t);
-//	private final KStructType.StructFieldType adt1_f = KStructType.sft("adt1", float_t);
-//	private final KStructType.StructFieldType adt2_f = KStructType.sft("adt2", float_t);
 	private final KStructType input_struct_t
 		= new KStructType(
 				KStructType.sft("x1", array2_t),
@@ -47,7 +42,7 @@ public class ResCalcKernel extends Kernel {
 		= new KStructType(
 				KStructType.sft("res1", array4_t),
 				KStructType.sft("res2", array4_t)
-		);
+			);
 
 
 	public ResCalcKernel(KernelParameters params) {
@@ -65,9 +60,10 @@ public class ResCalcKernel extends Kernel {
 
 		Counter nhd1_in_count = control.count.makeCounter(count_params);
 
-		SMIO read_from_host = addStateMachine("host_read", new ResInputSM(this, 10));
+		SMIO read_from_host_sm = addStateMachine("host_read", new ResInputSM(this, 10));
+		HWVar read_from_host = read_from_host_sm.getOutput("output");
 		KStruct input_data_dram = io.input("input_dram", input_struct_t);
-		KStruct input_data_host = io.input("input_host", input_struct_t, read_from_host.getOutput("output"));
+		KStruct input_data_host = io.input("input_host", input_struct_t, read_from_host);
 
 		HWVar gm1 = io.scalarInput("gm1", float_t);
 		HWVar eps = io.scalarInput("eps", float_t);
@@ -83,12 +79,73 @@ public class ResCalcKernel extends Kernel {
 
 		KStruct ram_output = mem.ramDualPort(partition_size, RamWriteMode.READ_FIRST, ram_params_write, ram_params_read).getOutputB();
 
-		KArray<HWVar> x1 = ram_output["x1"];
-		KArray<HWVar> x2 = ram_output["x2"];
-		KArray<HWVar> q1 = ram_output["q1"];
-		KArray<HWVar> q2 = ram_output["q2"];
-		HWVar adt1 = ram_output["adt1"];
-		HWVar adt2 = ram_output["adt2"];
+
+
+		Count.Params host_ram_write_count_params = control.count.makeParams(MathUtils.bitsToAddress(halo_size))
+													.withEnable(read_from_host);
+		Counter host_ram_write_count = control.count.makeCounter(host_ram_write_count_params);
+		RamPortParams<KStruct> host_ram_params_write = mem.makeRamPortParams(RamPortMode.WRITE_ONLY, host_ram_write_count .getCount(), input_data_host.getType())
+														.withDataIn(input_data_host)
+														.withWriteEnable(read_from_host);
+		Count.Params host_ram_read_count_params = control.count.makeParams(MathUtils.bitsToAddress(halo_size));
+		Counter host_ram_read_count = control.count.makeCounter(host_ram_read_count_params);
+		RamPortParams<KStruct> host_ram_params_read = mem.makeRamPortParams(RamPortMode.READ_ONLY, host_ram_read_count.getCount(), input_data_dram.getType());
+
+		KStruct host_ram_output = mem.ramDualPort(halo_size, RamWriteMode.READ_FIRST, host_ram_params_write, host_ram_params_read).getOutputB();
+
+		KStruct result_dram = doResMath(ram_output, eps, gm1);
+		KStruct result_host = doResMath(host_ram_output, eps, gm1);
+
+//		KArray<HWVar> x1 = ram_output["x1"];
+//		KArray<HWVar> x2 = ram_output["x2"];
+//		KArray<HWVar> q1 = ram_output["q1"];
+//		KArray<HWVar> q2 = ram_output["q2"];
+//		HWVar adt1 = ram_output["adt1"];
+//		HWVar adt2 = ram_output["adt2"];
+//		HWVar mu = 0.5f*(adt1+adt2)*eps;
+//
+//		HWVar dx = x1[0] - x2[0];
+//		HWVar dy = x1[1] - x2[1];
+//		HWVar ri = 1.0f / q1[0];
+//		HWVar p1 = gm1 * (q1[3] - 0.5f*ri*( q1[1] * q1[1] + q1[2] * q1[2]) );
+//		HWVar vol1 = ri * (q1[1]*dy - q1[2]*dx);
+//
+//		ri = 1.0f / q1[0];
+//		HWVar p2 = gm1*(q2[3]-0.5f*ri*(q2[1]*q2[1]+q2[2]*q2[2]));
+//		HWVar vol2 = ri*(q2[1]*dy - q2[2]*dx);
+//
+//		KStruct result = res_struct_t.newInstance(this);
+//		KArray<HWVar> res1 = result["res1"];
+//		KArray<HWVar> res2 = result["res2"];
+//
+//		HWVar f = 0.5f*(vol1* q1[0] + vol2* q2[0]) + mu*(q1[0]-q2[0]);
+//		res1[0] <== f;
+//		res2[0] <== -f;
+//
+//		f = 0.5f*(vol1* q1[1] + p1*dy + vol2* q2[1] + p2*dy) + mu*(q1[1]-q2[1]);
+//		res1[1] <== f;
+//		res2[1] <== -f;
+//
+//		f = 0.5f*(vol1* q1[2] - p1*dx + vol2* q2[2] - p2*dx) + mu*(q1[2]-q2[2]);
+//		res1[2] <== f;
+//		res2[2] <== -f;
+//
+//		f = 0.5f*(vol1*(q1[3]+p1)     + vol2*(q2[3]+p2)    ) + mu*(q1[3]-q2[3]);
+//		res1[3] <== f;
+//		res2[3] <== -f;
+
+		io.output("result_dram", result_dram.getType()) <== result_dram;
+		io.output("result_host", result_host.getType()) <== result_host;
+	}
+
+	KStruct doResMath(KStruct input_data, HWVar eps, HWVar gm1){
+
+		KArray<HWVar> x1 = input_data["x1"];
+		KArray<HWVar> x2 = input_data["x2"];
+		KArray<HWVar> q1 = input_data["q1"];
+		KArray<HWVar> q2 = input_data["q2"];
+		HWVar adt1 = input_data["adt1"];
+		HWVar adt2 = input_data["adt2"];
 		HWVar mu = 0.5f*(adt1+adt2)*eps;
 
 		HWVar dx = x1[0] - x2[0];
@@ -121,9 +178,8 @@ public class ResCalcKernel extends Kernel {
 		res1[3] <== f;
 		res2[3] <== -f;
 
-		io.output("result", result.getType()) <== result;
+		return result;
+
 	}
-
-
 
 }
