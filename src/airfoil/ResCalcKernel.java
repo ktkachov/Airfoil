e63d111997e7f7ed9d1c1d496669dec35940a348
Kyrill Tkachov <kyrilltkachov@yandex.ru>
2012-01-09 10:57:04 +0000

    cosmetic changes

--- src/airfoil/ResCalcKernel.java
+++ src/airfoil/ResCalcKernel.java
@@ -9,7 +9,6 @@ import com.maxeler.maxcompiler.v1.kernelcompiler.KernelParameters;
 import com.maxeler.maxcompiler.v1.kernelcompiler.SMIO;
 import com.maxeler.maxcompiler.v1.kernelcompiler.stdlib.core.Count;
 import com.maxeler.maxcompiler.v1.kernelcompiler.stdlib.core.Count.Counter;
-import com.maxeler.maxcompiler.v1.kernelcompiler.stdlib.core.Count.WrapMode;
 import com.maxeler.maxcompiler.v1.kernelcompiler.stdlib.core.Mem.RamPortMode;
 import com.maxeler.maxcompiler.v1.kernelcompiler.stdlib.core.Mem.RamPortParams;
 import com.maxeler.maxcompiler.v1.kernelcompiler.stdlib.core.Mem.RamWriteMode;
@@ -51,14 +50,7 @@ public class ResCalcKernel extends Kernel {
 		HWVar nhd1Size = io.scalarInput("nhd1Size", input_data_count_t);
 		HWVar nhd2Size = io.scalarInput("nhd2Size", input_data_count_t);
 		HWVar intraHaloSize = io.scalarInput("intraHaloSize", input_data_count_t);
-
 		HWVar haloDataSize = io.scalarInput("halo_size", input_data_count_t);
-		Counter host_halo_counter = control.count.makeCounter(control.count.makeParams(4).withMax(10));
-		Count.Params count_params = control.count.makeParams(input_data_count_width)
-									.withWrapMode(WrapMode.STOP_AT_MAX)
-									.withMax(nhd1Size);
-
-		Counter nhd1_in_count = control.count.makeCounter(count_params);
 
 		SMIO read_from_host_sm = addStateMachine("host_read", new ResInputSM(this, 10));
 		HWVar read_from_host = read_from_host_sm.getOutput("output");
@@ -84,7 +76,7 @@ public class ResCalcKernel extends Kernel {
 		Count.Params host_ram_write_count_params = control.count.makeParams(MathUtils.bitsToAddress(halo_size))
 													.withEnable(read_from_host);
 		Counter host_ram_write_count = control.count.makeCounter(host_ram_write_count_params);
-		RamPortParams<KStruct> host_ram_params_write = mem.makeRamPortParams(RamPortMode.WRITE_ONLY, host_ram_write_count .getCount(), input_data_host.getType())
+		RamPortParams<KStruct> host_ram_params_write = mem.makeRamPortParams(RamPortMode.WRITE_ONLY, host_ram_write_count.getCount(), input_data_host.getType())
 														.withDataIn(input_data_host)
 														.withWriteEnable(read_from_host);
 		Count.Params host_ram_read_count_params = control.count.makeParams(MathUtils.bitsToAddress(halo_size));
@@ -95,44 +87,6 @@ public class ResCalcKernel extends Kernel {
 
 		KStruct result_dram = doResMath(ram_output, eps, gm1);
 		KStruct result_host = doResMath(host_ram_output, eps, gm1);
-
-//		KArray<HWVar> x1 = ram_output["x1"];
-//		KArray<HWVar> x2 = ram_output["x2"];
-//		KArray<HWVar> q1 = ram_output["q1"];
-//		KArray<HWVar> q2 = ram_output["q2"];
-//		HWVar adt1 = ram_output["adt1"];
-//		HWVar adt2 = ram_output["adt2"];
-//		HWVar mu = 0.5f*(adt1+adt2)*eps;
-//
-//		HWVar dx = x1[0] - x2[0];
-//		HWVar dy = x1[1] - x2[1];
-//		HWVar ri = 1.0f / q1[0];
-//		HWVar p1 = gm1 * (q1[3] - 0.5f*ri*( q1[1] * q1[1] + q1[2] * q1[2]) );
-//		HWVar vol1 = ri * (q1[1]*dy - q1[2]*dx);
-//
-//		ri = 1.0f / q1[0];
-//		HWVar p2 = gm1*(q2[3]-0.5f*ri*(q2[1]*q2[1]+q2[2]*q2[2]));
-//		HWVar vol2 = ri*(q2[1]*dy - q2[2]*dx);
-//
-//		KStruct result = res_struct_t.newInstance(this);
-//		KArray<HWVar> res1 = result["res1"];
-//		KArray<HWVar> res2 = result["res2"];
-//
-//		HWVar f = 0.5f*(vol1* q1[0] + vol2* q2[0]) + mu*(q1[0]-q2[0]);
-//		res1[0] <== f;
-//		res2[0] <== -f;
-//
-//		f = 0.5f*(vol1* q1[1] + p1*dy + vol2* q2[1] + p2*dy) + mu*(q1[1]-q2[1]);
-//		res1[1] <== f;
-//		res2[1] <== -f;
-//
-//		f = 0.5f*(vol1* q1[2] - p1*dx + vol2* q2[2] - p2*dx) + mu*(q1[2]-q2[2]);
-//		res1[2] <== f;
-//		res2[2] <== -f;
-//
-//		f = 0.5f*(vol1*(q1[3]+p1)     + vol2*(q2[3]+p2)    ) + mu*(q1[3]-q2[3]);
-//		res1[3] <== f;
-//		res2[3] <== -f;
 
 		io.output("result_dram", result_dram.getType()) <== result_dram;
 		io.output("result_host", result_host.getType()) <== result_host;
